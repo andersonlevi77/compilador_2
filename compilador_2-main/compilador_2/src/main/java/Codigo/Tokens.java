0c0092fe7f0f4b6a6b6e26f94b4c49e5fe299710
Juan Díaz <jchendiaz15@gmail.com>
2024-02-05 20:19:23 -0600

    configuración expresión regular

--- compilador_2-main/compilador_2/src/main/java/Codigo/Tokens.java
+++ compilador_2-main/compilador_2/src/main/java/Codigo/Tokens.java
@@ -13,12 +13,11 @@ public class Tokens {
     public ArrayList<String> separacionTokens(String texto) {
         ArrayList<String> tokens = new ArrayList<>();
         // Actualiza el patrón para incluir los saltos de línea en el manejo de tokens
-        Pattern pattern = Pattern.compile("//[^\\n]*|/\\*.*?\\*/|\\n+|[a-zA-Z]+\\d*|[0-9.]+|\\;|\\=|\\+\\+|\\-\\-|\\+|\\-|\\)|\\(", Pattern.DOTALL);
+        Pattern pattern = Pattern.compile("//[^\\n]*|/\\*.*?\\*/|\\n+|[a-zA-Z][a-zA-Z0-9_$]*|\\d+(\\.\\d+)?|\\;|\\=|\\+\\+|\\-\\-|\\+|\\-|\\)|\\(",Pattern.DOTALL);
         Matcher matcher = pattern.matcher(texto);
-
+        
         int numeroLinea = 1;
         int inicioLinea = 0;
-
         while (matcher.find()) {
             String token = matcher.group();
 
